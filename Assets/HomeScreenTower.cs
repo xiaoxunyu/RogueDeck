ec2826b534abc51d03d4f7bc90a9949807685358
Xiaoxun Yu <xiaoxun.yu@pocketgems.com>
2019-09-12 13:41:40 -0700

    Finished home screen

--- Assets/HomeScreenTower.cs
+++ Assets/HomeScreenTower.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class HomeScreenArrow : MonoBehaviour
+public class HomeScreenTower : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +15,10 @@ public class HomeScreenArrow : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene("StartingSquadScreen");
     }
 }
---
+++ Assets/Scripts/HomeScreenTowerList.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeScreenTowerList : MonoBehaviour
+{
+	int currentSelection = 0;
+    float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = transform.GetChild(1).position.x - transform.GetChild(0).position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Scroll(bool toLeft){
+		if (toLeft)
+        {
+            if (currentSelection < transform.childCount - 1)
+            {
+                currentSelection++;
+                transform.position -= new Vector3(distance, 0);
+            }
+        }
+        else
+        {
+            if (currentSelection > 0)
+            {
+                currentSelection--;
+                transform.position += new Vector3(distance, 0);
+            }
+        }
+    }
+}
