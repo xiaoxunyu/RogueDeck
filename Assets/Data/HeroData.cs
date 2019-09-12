6da486473ef7cfa4dd1777a291da63f1f9a12a65
Xiaoxun Yu <xiaoxun.yu@pocketgems.com>
2019-09-12 14:43:42 -0700

    Finished home tabs

---
+++ Assets/Data/HeroData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Hero", menuName = "ScriptableObjects/Hero", order = 1)]
+public class HeroData : ScriptableObject
+{
+    public string Name;
+    public Sprite Portrait;
+
+    public int HP;
+    public int Attack;
+    public int Range;
+}
---
+++ Assets/Scripts/HomeScreenTabManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeScreenTabManager : MonoBehaviour
+{
+    public Transform CityScreen;
+    public Transform BattleScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickCity()
+    {
+        BattleScreen.gameObject.SetActive(false);
+        CityScreen.gameObject.SetActive(true);
+        CityScreen.localPosition = Vector3.zero;
+    }
+
+    public void OnClickBattle()
+    {
+        CityScreen.gameObject.SetActive(false);
+        BattleScreen.gameObject.SetActive(true);
+    }
+}
