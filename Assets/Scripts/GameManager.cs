7bf7145468ef78fb6bdd60c4a35b12ce23b0a55b
Xiaoxun Yu <xiaoxun.yu@pocketgems.com>
2019-09-12 15:00:25 -0700

    Added something

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static List<HeroData> heroDataList = new List<HeroData>();
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+
+        heroDataList.Add((HeroData)Resources.Load("Sniper"));
+        print(heroDataList[0].Range);
+    }
+
+    void Update()
+    {
+        
+    }
+}
