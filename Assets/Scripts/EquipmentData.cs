f234f71d3feeca4167df0032bbc59df692bc1fb3
frozenwaffles <andy@pocketgems.com>
2019-09-12 19:04:54 -0700

    Created eqiupment data and some equipment and updated images on the heroinfo tab

---
+++ Assets/Scripts/EquipmentData.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Equipment", menuName = "ScriptableObjects/Equipment", order = 1)]
+public class EquipmentData : ScriptableObject
+{
+    public string Name;
+    public Sprite Portrait;
+
+    public int HP;
+    public int Attack;
+    public int Range;
+    public string Spell1Name;
+}
