39f59b33380817cf6888629f26f0483f718e79af
frozenwaffles <andy@pocketgems.com>
2019-09-12 17:29:25 -0700

    Updated inside house screen and made a hero info prefab

---
+++ Assets/Scripts/InsideHouseHeroButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InsideHouseHeroButton : MonoBehaviour
+{
+    public HeroData hero;
+    private Image buttonImage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonImage = gameObject.GetComponent<Image>();
+        if (hero != null) {
+            buttonImage.sprite = hero.Portrait;
+        } else {
+            gameObject.SetActive(false);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
