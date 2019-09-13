e5391fa03d168d30ff1cc64e3910f3d470fe60a3
frozenwaffles <andy@pocketgems.com>
2019-09-12 21:08:48 -0700

    Adding hero info popup with close button

---
+++ Assets/Scripts/HeroInfoButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroInfoButton : MonoBehaviour
+{
+    public GameObject heroInfoPrefab;
+    private GameObject myPanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick() {
+        if(myPanel == null) {
+            myPanel  = Instantiate(heroInfoPrefab, this.transform.position, this.transform.rotation) as GameObject;
+            myPanel.transform.SetParent(gameObject.transform.root,false);
+            //myPanel.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+    }
+}
