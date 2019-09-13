ecafb18b0e412c5811e256e9e3b0182f4dea5307
Xiaoxun Yu <xiaoxun.yu@pocketgems.com>
2019-09-13 14:56:51 -0700

    Real final version

---
+++ Assets/Scripts/MapIconList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapIconList : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < GameManager.BattleCount; i++)
+            transform.GetChild(i + 1).gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
