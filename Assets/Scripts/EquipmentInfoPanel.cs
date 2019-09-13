e91d2234c4968fc08fa79f4702063d69c8eb8513
frozenwaffles <andy@pocketgems.com>
2019-09-12 19:44:11 -0700

    Added logic to equipment prefab

---
+++ Assets/Scripts/EquipmentInfoPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EquipmentInfoPanel : MonoBehaviour {
+
+    public EquipmentData equip;
+    public TextMeshProUGUI attack;
+    public TextMeshProUGUI hp;
+    public TextMeshProUGUI range;
+    public TextMeshProUGUI spell;
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        attack.text = equip.Attack.ToString();
+        hp.text = equip.HP.ToString();
+        range.text = equip.Range.ToString();
+        spell.text = equip.Spell1Name;
+        image.sprite = equip.Portrait;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
+
