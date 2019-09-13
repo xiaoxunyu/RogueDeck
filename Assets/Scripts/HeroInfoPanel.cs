b4ca6cb3ffe3045c66bff7f992ee47d3fd999541
frozenwaffles <andy@pocketgems.com>
2019-09-12 19:37:32 -0700

    Added logic for hero info panel

---
+++ Assets/Scripts/HeroInfoPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HeroInfoPanel : MonoBehaviour
+{
+    public HeroData hero;
+    public TextMeshProUGUI attack;
+    public TextMeshProUGUI hp;
+    public TextMeshProUGUI range;
+    public TextMeshProUGUI spell;
+    public Image image;  
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        attack.text = hero.Attack.ToString();
+        hp.text = hero.HP.ToString();
+        range.text = hero.Range.ToString();
+        spell.text = hero.Spell1Name;
+        image.sprite = hero.Portrait;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
